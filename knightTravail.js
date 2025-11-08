7bce7c7aadafd122e8d89cabf75e428c09fe9de2
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-11-08 17:42:31 +0500

    A for loop iterating for 8 times

--- knightTravail.js
+++ knightTravail.js
@@ -3,4 +3,8 @@ function getValidMoves(array) {
         [2,1],[2,-1],[-2,1],[-2,-1],   
         [1,2],[-1,2],[1,-2],[-1,-2]
     ];
+    let validMovesArray = [];
+    for(let i = 0; i < 8; i++) {
+        
+    }
 }
