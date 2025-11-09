00cf766dff0b31ed121647b5f5c1afed8038a30c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-11-09 19:40:46 +0500

    If arrays length are not equal then return false

--- knightTravail.js
+++ knightTravail.js
@@ -23,7 +23,7 @@ function getValidMoves(array) {
 function areArraysEqual(array1, array2) {
     if(array1 === array2) return true;
     if(array1 == null || array2 == null) return false;
-   
+   if(array1.length !== array2.length) return false;
 
    
 }
