f51e7cf4703ec74ac036edea90a5e8708f332ac0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-11-09 19:41:18 +0500

    Compare each element of two arrays and if they are equal return true

--- knightTravail.js
+++ knightTravail.js
@@ -25,6 +25,10 @@ function areArraysEqual(array1, array2) {
     if(array1 == null || array2 == null) return false;
    if(array1.length !== array2.length) return false;
 
-   
+   for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) return false;
+   }
+
+   return true;
 }
 
